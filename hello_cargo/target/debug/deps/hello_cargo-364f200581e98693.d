b5308c1b6a24b8dabdfc8501688f3ead0a176c3f
BuccWheat <BuckWheatbatyr@gmail.com>
2021-07-21 15:14:45 -0500

    files added from official tutorial docs. progress up to chapter 2 (midway through).

---
+++ hello_cargo/target/debug/deps/hello_cargo-364f200581e98693.d
@@ -0,0 +1,5 @@
+/home/buccwheat/Documents/rust_learning/hello_cargo/target/debug/deps/hello_cargo-364f200581e98693: src/main.rs
+
+/home/buccwheat/Documents/rust_learning/hello_cargo/target/debug/deps/hello_cargo-364f200581e98693.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/buccwheat/Documents/rust_learning/hello_cargo/target/debug/hello_cargo: /home/buccwheat/Documents/rust_learning/hello_cargo/src/main.rs
